f416b2872ef497817dbc6d21a27d10afdb775f3f
Martin Prokoph <motschen@midnightdust.eu>
2024-10-18 22:15:48 +0200

    Get PWA support working

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,5 +11,43 @@ export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false
-  }), AstroPWA()],
+  }), AstroPWA({
+    mode: "production",
+    base: "/",
+    scope: "/",
+    includeAssets: ["favicon.png"],
+    registerType: "autoUpdate",
+    manifest: {
+      name: "HubTWK – Tools für Studierende",
+      short_name: "HubTWK",
+      theme_color: "#121212",
+      icons: [
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: "/404",
+      globPatterns: ["*.js"],
+    },
+    devOptions: {
+      enabled: false,
+      navigateFallbackAllowlist: [/^\/404$/],
+      suppressWarnings: true,
+    },
+  }),],
 });
