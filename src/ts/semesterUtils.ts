d5356af50b2e1af3fe3c9e1fa64d31b657d80433
Martin Prokoph <motschen@midnightdust.eu>
2025-03-29 17:57:10 +0100

    feat: add support for summer semesters

---
+++ src/ts/semesterUtils.ts
@@ -0,0 +1,26 @@
+import { getISOWeek } from "date-fns";
+
+export function isSummerSemester(): boolean {
+    return new Date().getMonth() >= 2 && new Date().getMonth() <= 8;
+}
+
+export function isSecondYearOfWinterSemester(): boolean {
+    return new Date().getMonth() <= 1;
+}
+
+export function getSemesterStartYear(): number {
+    return (isSecondYearOfWinterSemester() ? new Date().getFullYear() - 1 : new Date().getFullYear());
+}
+
+export function getCurrentWeek(): number {
+    let currentWeek = getISOWeek(new Date());
+    if (isSecondYearOfWinterSemester()) currentWeek += 52; // Planungswochen werden auch nach dem Jahreswechsel fortlaufend nummeriert
+    return currentWeek;
+}
+
+export function currentSemesterName(): string {
+    let semesterName = (isSummerSemester() ? "Sommersemester " : "Wintersemester ");
+    if (!isSummerSemester()) semesterName += getSemesterStartYear() + "/" + (getSemesterStartYear() + 1).toString().substring(2);
+    else semesterName += getSemesterStartYear();
+    return semesterName;
+}
